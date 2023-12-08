84185c4c91e9b0d890fd720cd548a432f4dc1348
Lukas Juskevicius <ljuskevicius2002@gmail.com>
2023-12-08 15:48:31 +0100

    test application re-added

---
+++ webApplication.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Xunit;
+
+namespace webApplication.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(1 == 1);
+        }
+    }
+}
+
